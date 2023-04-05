1314ae35647fa3ae70d4c2ea5a01dd93ec77e323
vamins <106886439+vamins@users.noreply.github.com>
2023-04-05 14:55:40 -0400

    Add files via upload

---
+++ circurferencia.py
@@ -0,0 +1,23 @@
+pi = 3.141592
+a = 3
+b = 8
+C = 10
+x = 1
+
+if x == 1:
+    c = 2*pi*a
+    print("la circurferencia con radio 3 es:",c)
+    x += 1
+
+if x == 2:
+    c = 2*pi*b
+    print("la circurferencia con radio 3 es:",c)
+    x += 1
+
+if x == 3:
+    c = 2*pi*C
+    print("la circurferencia con radio 3 es:",c)
+
+def Listnotas:
+
+return Listnotas
